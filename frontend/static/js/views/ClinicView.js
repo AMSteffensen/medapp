6e73423b24629e16bf5793a42653e8731cf78cf1
steand <andreas.steffensen@n5n.no>
2021-06-03 20:26:11 +0200

    loop through periods

--- frontend/static/js/views/ClinicView.js
+++ frontend/static/js/views/ClinicView.js
@@ -1,42 +1,28 @@
 import AbstractView from "./AbstractView.js";
 
-const API_URL = "https://staging-core.api.drdropin.no/v1/clinics ";
-
-const fetchClinicView = async () => {
-  fetch(API_URL)
-    .then((resp) => resp.json())
-    .then(function (data) {
-      let openingHours = data.openingHours;
-      console.log(data);
-      return clinics.map(function (clinic) {
-        console.log(clinic.name);
-        console.log(clinic.openingHours);
-
-        const div = document.createElement("div");
-        const h4 = document.createElement("h4");
-        const a = document.createElement("a");
-
-        const body = document.querySelector("body");
-        body.append(div);
-        h4.append(a);
-        div.append(h4);
-
-        // Set content and attributes
-        a.innerHTML = clinic.name;
-        a.setAttribute("href", "/clinics/" + clinic.id);
-        div.setAttribute("class", "card");
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-};
-
 export default class extends AbstractView {
+  API_URL = "";
+
   constructor(params) {
     super(params);
     this.clinicId = params.id;
     this.setTitle("Viewing clinic");
+
+    fetch("https://staging-core.api.drdropin.no/v1/clinics/" + this.clinicId)
+      .then((resp) => resp.json())
+      .then(function (data) {
+        let bookingHours = data.bookingHours;
+        let periods = data.bookingHours.periods;
+
+        Object.keys(bookingHours).map((item) => {
+          var value = bookingHours[item];
+          console.log(value.periods);
+          // loop through the periods
+          for (let [key, val] of Object.entries(value.periods)) {
+            console.log(key, "--", val);
+          }
+        });
+      });
   }
 
   async getHtml() {
