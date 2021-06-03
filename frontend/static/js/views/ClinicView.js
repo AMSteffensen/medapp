75a45da38cb630bef1bb3af865e9f2af2896bfab
steand <andreas.steffensen@n5n.no>
2021-06-03 09:18:29 +0200

    update clinic view to show id

--- frontend/static/js/views/ClinicView.js
+++ frontend/static/js/views/ClinicView.js
@@ -1,10 +1,42 @@
 import AbstractView from "./AbstractView.js";
 
+const API_URL = "https://staging-core.api.drdropin.no/v1/clinics ";
+
+const fetchClinicView = async () => {
+  fetch(API_URL)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      let openingHours = data.openingHours;
+      console.log(data);
+      return clinics.map(function (clinic) {
+        console.log(clinic.name);
+        console.log(clinic.openingHours);
+
+        const div = document.createElement("div");
+        const h4 = document.createElement("h4");
+        const a = document.createElement("a");
+
+        const body = document.querySelector("body");
+        body.append(div);
+        h4.append(a);
+        div.append(h4);
+
+        // Set content and attributes
+        a.innerHTML = clinic.name;
+        a.setAttribute("href", "/clinics/" + clinic.id);
+        div.setAttribute("class", "card");
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
 export default class extends AbstractView {
   constructor(params) {
     super(params);
-    this.postId = params.id;
-    this.setTitle("Viewing Clinics");
+    this.clinicId = params.id;
+    this.setTitle("Viewing clinic");
   }
 
   async getHtml() {
--- frontend/static/js/views/Clinics.js
+++ frontend/static/js/views/Clinics.js
@@ -2,14 +2,6 @@ import AbstractView from "./AbstractView.js";
 
 const API_URL = "https://staging-core.api.drdropin.no/v1/clinics ";
 
-function createNode(element) {
-  return document.createElement(element);
-}
-
-function append(parent, el) {
-  return parent.appendChild(el);
-}
-
 const ul = document.getElementById("clinics");
 
 const fetchClinicData = async () => {
@@ -21,7 +13,6 @@ const fetchClinicData = async () => {
       return clinics.map(function (clinic) {
         console.log(clinic.name);
         console.log(clinic.openingHours);
-        let li = createNode("li");
 
         const div = document.createElement("div");
         const h4 = document.createElement("h4");
@@ -34,24 +25,8 @@ const fetchClinicData = async () => {
 
         // Set content and attributes
         a.innerHTML = clinic.name;
-        a.setAttribute("href", clinic.name);
-        //img.setAttribute("src", clinic.id);
+        a.setAttribute("href", "/clinics/" + clinic.id);
         div.setAttribute("class", "card");
-
-        // let span = createNode("span");
-        // let a = createNode("a");
-        // let linkText = document.createTextNode(`${clinic.name}`);
-        // a.title = `${clinic.name}`;
-        // a.href = `${clinic.id}`;
-        // li.appendChild(a);
-
-        //append(li, a);
-
-        //a.innerHTML = `${clinic.name}`;
-        //append(a, linkText);
-        //append(l);
-        //append(li, span);
-        //append(ul, li);
       });
     })
     .catch(function (error) {
@@ -69,8 +44,7 @@ export default class extends AbstractView {
   async getHtml() {
     return `       
             <h1>Clinics</h1>
-         
-            
+              
         `;
   }
 }
